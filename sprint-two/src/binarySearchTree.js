5adebbdd4c62d8723c5c669c1fc8a2016804598b
Hack Reactor Student <student@hackreactor.com>
2017-04-01 17:34:21 -0700

    Binary search tree in progress

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,20 @@
 var BinarySearchTree = function(value) {
+	this.value = value;
+	this.left = {};
+	this.right = {};
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+	// body...
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+	// body...
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(value) {
+	// body...
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
